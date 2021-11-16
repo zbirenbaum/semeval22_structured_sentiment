1dd5aff3293348ea7f8b9a0b9f39384d36691307
zbirenbaum <zacharyobirenbaum@gmail.com>
2021-11-15 22:22:48 -0500

    "df update"

--- modelsrc/classifier.py
+++ modelsrc/classifier.py
@@ -1,41 +1,7 @@
-import argparse
-import json
+from df_builder import build_df
 import pandas as pd
 
-#each dict contains sent id, text, opinion
-#['opinions'] contains a list of dicts
-#['opinions'] can be of arbitrary len including 0
-dataset = "../data/opener_en/train.json"
-with open(dataset) as f:
-    data=json.load(f)
 
-opinion_labels = ['Polar_expression', 'Intensity', 'Source', 'Polarity', 'Target']
-labels = ['sent_id', 'text']
-
-def build_clean_df():
-    rows = []
-    for datapoint in data:
-        basis = {}
-        to_append = {}
-        for label in labels:
-            basis[label] = str(datapoint[label])
-            
-        if len(datapoint['opinions']) != 0:
-            for opinion in datapoint['opinions']:
-                for label in opinion_labels:
-                    try:
-                        to_append[label] = opinion[label]
-                    except:
-                        to_append[label] = None
-                for key in basis.keys():
-                    to_append[key] = basis[key]
-                rows.append(to_append.copy())
-                to_append = basis
-        else:
-            for label in opinion_labels:
-                to_append[label] = None
-    df = pd.DataFrame(rows)
-    return df
-
-df = build_clean_df()
+df = build_df()
 print(df)
+
